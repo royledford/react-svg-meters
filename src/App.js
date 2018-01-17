6ea2ecd05e07ace6dafe6b7f7431ca1ba1622497
roy ledford <bluesixty@gmail.com>
2018-01-17 09:55:35 -0500

    re-org folder structures

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './components/Home'
-import Meters from './components/Meters'
-
-import './App.css'
+import Home from './docs/Home'
+import Meters from './docs/Meters'
 
 class App extends Component {
   render() {
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   ignore: ['**/src/components/Home.js', '**/*.test.js', '**/components/Meters.js'],
+  template: './styleguide/template.html',
 }
