ae14ce4c77b44c630dae717ca464240a42a26b80
roy ledford <bluesixty@gmail.com>
2018-01-30 16:48:54 -0500

    Update examples to show nicer text for the name

--- src/docs/Example.js
+++ src/docs/Example.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import CodeExample from './CodeExample'
+import { camelToNormalCase } from '../helpers/helpers'
 import './Example.css'
 
 class Example extends React.Component {
@@ -20,16 +21,19 @@ class Example extends React.Component {
     const { showCode } = this.state
     const { code, description, name } = this.props.example
 
+    const exampleName = camelToNormalCase(name)
+
     const codeDisplay = showCode ? '' : 'example--code-hide'
 
     let descriptionArray = description ? description.split('\n\n') : null
 
     // Must use CommonJS require to dynamically require because ES Modules must be statically analyzable.
     const ExampleComponent = require(`./examples/${this.props.componentName}/${name}`).default
+
     return (
       <section className="example">
         <div className="example--header" onClick={this.toggleCode}>
-          <span className="example--name"> {name}</span>
+          <span className="example--name"> {exampleName}</span>
           <span className="example--codelink">{`{ }`}</span>
         </div>
         <CodeExample className={`example--code ${codeDisplay}`}>{code}</CodeExample>
