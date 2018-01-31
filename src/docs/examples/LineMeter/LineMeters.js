8dcf13dd759739098ee17e90629d09d4691e405c
roy ledford <bluesixty@gmail.com>
2018-01-31 10:02:25 -0500

    Update docs and examples for line meter

--- src/docs/examples/LineMeter/LineMeters.js
+++ src/docs/examples/LineMeter/LineMeters.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { HalfCircleMeter } from 'react-svg-meters'
+import { LineMeter } from 'react-svg-meters'
 
 /**
- * You can change the individual colors of the meter, and change
- * the text styles in the disk meter. Rounded ends can be applied using by setting
+ * You can change the individual colors of the meter, the text position, and change
+ * the text styles in the meter. Rounded ends can be applied by setting
  * the rounded property to true.
  *
  * By default the text color will match the foregroundColor. This can be changed with
- * the text color porperty.
+ * the text color property.
  *
  * The text can changed using the textStyle property and providing a style object.
  * It is svg text and fill is used to change the text color.
+ *
+ * The second example shows the text placed on the bottom by setting the
+ * position property to 'bottom'
  */
-export default function HalfcircleMeters() {
+export default function LineMeters() {
   const styles = {
     meter: {
       margin: 8,
@@ -25,38 +28,38 @@ export default function HalfcircleMeters() {
     textStyle: {
       fontFamily: 'sans-serif',
       fill: '#1FC939',
-      fontSize: 24,
+      fontSize: 70,
     },
   }
 
   return (
     <div>
-      <HalfCircleMeter value={35} size={150} style={styles.meter} />
-      <HalfCircleMeter
+      <LineMeter value={35} size={150} style={styles.meter} />
+      <LineMeter
         value={75}
         size={150}
+        position="top"
+        thickness={56}
+        foregroundColor={styles.color.foreground}
+        backgroundColor={styles.color.background}
+        style={styles.meter}
+      />
+      <LineMeter
+        value={55}
+        size={150}
+        thickness={16}
         rounded={true}
         foregroundColor={styles.color.foreground}
         backgroundColor={styles.color.background}
         style={styles.meter}
       />
-      <HalfCircleMeter
-        value={55}
-        size={150}
-        thickness={36}
-        foregroundColor={styles.color.foreground}
-        backgroundColor={styles.color.background}
-        textColor={styles.color.bright}
-        style={styles.meter}
-      />
-      {/* The text in the meter is svg text so use the fill style to adjust the color */}
-      <HalfCircleMeter
+      <LineMeter
         value={65}
         size={150}
-        thickness={14}
+        rounded={true}
+        textStyle={styles.textStyle}
         foregroundColor={styles.color.foreground}
         backgroundColor={styles.color.background}
-        textStyle={styles.textStyle}
         style={styles.meter}
       />
     </div>
